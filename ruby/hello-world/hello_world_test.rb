70526d48f557dcf84afae6f14c51a044aafd4ed7
Linda Le <yueling1222@gmail.com>
2020-03-15 14:36:40 -0600

    Add ruby hello world response

--- ruby/hello-world/hello_world_test.rb
+++ ruby/hello-world/hello_world_test.rb
@@ -1,6 +1,7 @@
 begin
   gem 'minitest', '>= 5.0.0'
   require 'minitest/autorun'
+  require 'minitest/pride'
   require_relative 'hello_world'
 rescue Gem::LoadError => e
   puts "\nMissing Dependency:\n#{e.backtrace.first} #{e.message}"
@@ -11,33 +12,9 @@ rescue LoadError => e
   exit 1
 end
 
-# Common test data version: 1.1.0 be3ae66
 class HelloWorldTest < Minitest::Test
   def test_say_hi
-    # skip
-    assert_equal "Hello, World!", HelloWorld.hello
+    assert_equal "Hello, World!", HelloWorld.print_hello
   end
 end
 
-__END__
-
-*****************************************************
-You got an error, which is exactly as it should be.
-This is the first step in the Test-Driven Development
-(TDD) process.
-
-The most important part of the error is
-
-   cannot load such file
-
-It's looking for a file named hello_world.rb that doesn't
-exist yet.
-
-To fix the error, create an empty file named hello_world.rb
-in the same directory as the hello_world_test.rb file.
-
-Then run the test again.
-
-For more guidance as you work on this exercise, see
-GETTING_STARTED.md.
-*****************************************************
